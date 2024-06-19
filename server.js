f325506cb1e035a610f9e4892447f384583962e6
Ngajimbajr <ngajimbajr@gmail.com>
2024-06-19 11:49:26 -0700

    alx-files_manager

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const controllerRouting = require('./routes/index');
+import express from 'express';
+import controllerRouting from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,5 +12,4 @@ app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-module.exports = app;
-
+export default app;
