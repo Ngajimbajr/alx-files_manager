de11d91acbaaee1e77821a10a83912d25f1612de
Ngajimbajr <ngajimbajr@gmail.com>
2024-06-19 08:24:50 -0700

    alx-files_manager

---
+++ utils/users.js
@@ -0,0 +1,14 @@
+// utils/users.js
+module.exports = {
+    // Placeholder function
+    getUserById: async (id) => {
+        // This is just a placeholder function.
+        // Replace this with actual database logic later.
+        return {
+            id,
+            name: "Placeholder User",
+            email: "user@example.com"
+        };
+    }
+};
+
